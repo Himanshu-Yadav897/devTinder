a8d62ecf9383657abd1d60a3188d24dffc53a09b
Himanshu Yadav <hy897433@gmail.com>
2025-03-31 20:40:42 +0530

    exploring the GET, POST etc..

--- src/app.js
+++ src/app.js
@@ -2,9 +2,16 @@ const express = require("express");
 
 const app = express();
 app.get("/", (req, res) => {
-  res.send("Hello World12!");
+  res.send("/");
 });
 
-app.listen(3000, () => {
+app.get("/hello/2", (req, res) => {
+  res.send("/hello/2");
+});
+
+app.post("/hello/2", (req, res) => {
+  res.send("Data Fetched Successfully");
+});
+app.listen(7777, () => {
   console.log("it's Running1");
 });
