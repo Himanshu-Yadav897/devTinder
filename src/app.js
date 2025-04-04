8137d281bd7b98a25c32a0e68467f337552f1439
Himanshu Yadav <hy897433@gmail.com>
2025-04-04 13:29:09 +0530

    multiple-route-handler

--- src/app.js
+++ src/app.js
@@ -1,17 +1,17 @@
 const express = require("express");
 
 const app = express();
-app.get("/", (req, res) => {
-  res.send("/");
-});
 
-app.get("/hello/2", (req, res) => {
-  res.send("/hello/2");
-});
-
-app.post("/hello/2", (req, res) => {
-  res.send("Data Fetched Successfully");
-});
+app.use(
+  "/user",
+  (req, res,next) => {
+    // res.send("response");
+    next();
+  },
+  (req, res) => {
+    res.send("response 2");
+  }
+);
 app.listen(7777, () => {
   console.log("it's Running1");
 });
