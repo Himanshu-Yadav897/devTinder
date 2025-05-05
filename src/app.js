f28286b5af98fb084f4ab772afdba62b26f6ea5a
Himanshu Yadav <hy897433@gmail.com>
2025-05-05 20:50:25 +0530

    API level data sanitization

--- src/app.js
+++ src/app.js
@@ -58,11 +58,33 @@ app.delete("/user", async (req, res) => {
 });
 
 // FindByID and Update using PATCH
-app.patch("/user", async (req, res) => {
-  const userId = req.body.userId;
+app.patch("/user/:userId", async (req, res) => {
+  const userId = req.params?.userId;
   const data = req.body;
 
   try {
+    // Api Level Validation
+    const Allowed_Updates = [
+      "password",
+      "age",
+      "gender",
+      "photoUrl",
+      "about",
+      "skills",
+    ];
+
+    const isUpdateAllowed = Object.keys(data).every((k) =>
+      Allowed_Updates.includes(k)
+    );
+
+    if (!isUpdateAllowed) {
+      throw new Error("Update not allowed");
+    }
+
+    if (data?.skills.length > 10) {
+      throw new Error("Skills cannot be more than 10");
+    }
+
     const user = await User.findByIdAndUpdate({ _id: userId }, data, {
       runValidators: true,
     });
